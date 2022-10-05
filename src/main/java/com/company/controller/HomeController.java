eabd6916c7ed8a4e2bd3e8864eb61106d60371ad
mdoli <mamsorea@gmail.com>
2022-10-05 23:03:58 +0900

    Initial commit

---
+++ src/main/java/com/company/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String showEmployeeHomePage() {
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mdoli
+  Date: 2022-10-04 0004
+  Time: 오후 9:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome!</title>
+</head>
+<body>
+  <h2>Hello HomePage</h2>
+</body>
+</html>
